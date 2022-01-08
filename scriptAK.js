e02f8031740af7e75b22d0eefe835c3781975847
Alexandra Kueck <alexandra.kueck@kkg.hamburg.de>
2022-01-08 15:23:40 +0100

    updtae46

--- scriptAK.js
+++ scriptAK.js
@@ -4,23 +4,6 @@ WA.onInit().then(() => {
 	WA.chat.sendChatMessage('Herzlich willkommen! ', 'Admin');
 	
 	
-
-	// Open the popup when we enter a given zone
-	helloWorldPopup = WA.room.onEnterLayer("silent").subscribe(() => {
-		WA.ui.openPopup("popupSilent", 'In der Silent-Zone!', [{
-			label: "schliessen",
-			className: "primary",
-			callback: (popup) => {
-				// Close the popup when the "Close" button is pressed.
-				popup.close();
-			}
-		}]);
-	});
-
-	// Close the popup when we leave the zone.
-	WA.room.onLeaveLayer("silent").subscribe(() => {
-		helloWorldPopup.close();
-	});
 	
 	// Open the popup when we enter a given zone
 	helloWorldPopup = WA.room.onEnterLayer("start").subscribe(() => {
